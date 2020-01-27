ed6680e1b6609b1b9691cead6f1fe10151f98f19
excavator <str_token@mail.ru>
2020-01-27 11:55:13 +0300

    shuring setup

--- 001_sendwich/sandwich_spec.rb
+++ 001_sendwich/sandwich_spec.rb
@@ -1,9 +1,16 @@
-Sandwich=Struct.new(:taste, :toppongs)
+Sandwich=Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
   it 'is delicious' do
     sandwich=Sandwich.new('delicious', [])
     taste=sandwich.taste
     expect(taste).to eq('delicious')
+  end
+  it "let me add toppings" do
+    sandwich=Sandwich.new('delicious', [])
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
+
+    expect(toppings).not_to be_empty
   end
 end
